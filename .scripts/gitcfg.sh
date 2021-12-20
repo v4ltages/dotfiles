803701f104726d188979cd9cb813e9ac751ef8aa
v4ltages <contact@voltages.me>
2021-12-20 21:24:21 +0200

    set to --global for now

--- .scripts/gitcfg.sh
+++ .scripts/gitcfg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ## Set git repo user and email to this
-git config user.email "contact@voltages.me"
-git config user.name "v4ltages"
+git config --global user.email "contact@voltages.me"
+git config --global user.name "v4ltages"
